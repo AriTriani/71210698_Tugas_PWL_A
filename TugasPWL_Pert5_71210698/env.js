30b222316461004efd49e6d752226736e379c3f5
AriTriani <93314786+AriTriani@users.noreply.github.com>
2023-10-02 10:35:39 +0700

    Add files via upload

---
+++ TugasPWL_Pert5_71210698/env.js
@@ -0,0 +1,12 @@
+// env.js
+const cat_env = {
+  API_KEY: "live_zHSrxD29ULHuPHzlGyQEth8H469znkFdxHy7C2dydPwzpRDnxNh738WA5a4XIE30",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+const dog_env = {
+  API_KEY: "live_zHSrxD29ULHuPHzlGyQEth8H469znkFdxHy7C2dydPwzpRDnxNh738WA5a4XIE30",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+export { cat_env, dog_env };
